f9e7893ca26fff686d69f56bc846134f917602bb
CICE <cice@AULA03MAC11.local>
2015-11-19 20:02:19 +0100

    finished the 3rd app

--- 003-AppStart/app/src/main/java/com/garcia/pablo/cice/appstart/MainActivity.java
+++ 003-AppStart/app/src/main/java/com/garcia/pablo/cice/appstart/MainActivity.java
@@ -1,7 +1,9 @@
 package com.garcia.pablo.cice.appstart;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * Lanza la activity DetailActivity.
+     * @param view Vista en la que se hizo click.
+     */
+    public void onClickMyButton(View view){
+        Intent intent = new Intent(MainActivity.this,DetailActivity.class);
+        startActivity(intent);
     }
 }
