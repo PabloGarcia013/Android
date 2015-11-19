ae90d0e4a8995f5cb0590326b5662c1e73f33f50
CICE <cice@AULA03MAC11.local>
2015-11-19 20:37:06 +0100

    finished the app number 3

--- 003-AppStart/app/src/main/java/com/garcia/pablo/cice/appstart/DetailActivity.java
+++ 003-AppStart/app/src/main/java/com/garcia/pablo/cice/appstart/DetailActivity.java
@@ -1,13 +1,30 @@
 package com.garcia.pablo.cice.appstart;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity {
+
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        // Cogemos el intent
+        Intent intent = getIntent();
+
+        // Cogemos el valor que pasamos en la MainActivity con el nombre que le dimos
+        String finalString = intent.getStringExtra("texto");
+
+        // Realizamos el binding del textView de la detailActivity
+        textView = (TextView) findViewById(R.id.finalTextView);
+
+        // Asignamos el texto al textView.
+        textView.setText(finalString);
+
     }
 }
--- 003-AppStart/app/src/main/java/com/garcia/pablo/cice/appstart/MainActivity.java
+++ 003-AppStart/app/src/main/java/com/garcia/pablo/cice/appstart/MainActivity.java
@@ -18,7 +18,20 @@ public class MainActivity extends AppCompatActivity {
      * @param view Vista en la que se hizo click.
      */
     public void onClickMyButton(View view){
+
+        /*
+            Creamos el intent que almacenará la información que queremos pasar.
+            Los Parametros son: Contexto de la app y activity a la que vamos a ir.
+        */
         Intent intent = new Intent(MainActivity.this,DetailActivity.class);
+
+        // Creamos un texto que queremos pasar.
+        String texto = "Este es el texto que pasamos a detail.";
+
+        // Añadimos el texto al intent para que lo pase a la nueva activity.
+        intent.putExtra("texto",texto);
+
+        // Iniciamos la nueva Actividad.
         startActivity(intent);
     }
 }
