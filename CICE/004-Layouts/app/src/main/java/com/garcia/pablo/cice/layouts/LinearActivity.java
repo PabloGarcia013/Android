a5a8c6355329d1020558376c9db7804d971437b3
CICE <cice@AULA03MAC11.local>
2015-11-24 19:05:15 +0100

    upload some changes

--- CICE/004-Layouts/app/src/main/java/com/garcia/pablo/cice/layouts/LinearActivity.java
+++ CICE/004-Layouts/app/src/main/java/com/garcia/pablo/cice/layouts/LinearActivity.java
@@ -4,16 +4,20 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
-public class LinearActivity extends AppCompatActivity {
+public class LinearActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_linear);
+        Button b = (Button) findViewById(R.id.goToRelavite2);
+        b.setOnClickListener(this);
     }
 
-    public void onClickGoToRelative(View view) {
+    @Override
+    public void onClick(View v) {
         Intent intent = new Intent(LinearActivity.this,RelativeActivity.class);
         this.startActivity(intent);
     }
