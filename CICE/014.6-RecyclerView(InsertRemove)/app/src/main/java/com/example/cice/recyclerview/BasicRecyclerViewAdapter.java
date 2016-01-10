fcab4d35680cc64b4fa3246a7d3e032e52cced2a
Pablo Garcia <V@MacBook-Pro-de-Pablo.local>
2016-01-10 19:42:59 +0100

    Insert remove row in Recycler view

---
+++ CICE/014.6-RecyclerView(InsertRemove)/app/src/main/java/com/example/cice/recyclerview/BasicRecyclerViewAdapter.java
@@ -0,0 +1,46 @@
+package com.example.cice.recyclerview;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+/**
+ * Created by CICE on 15/12/15.
+ */
+public class BasicRecyclerViewAdapter extends RecyclerView.Adapter<BasicViewHolder>{
+
+    ArrayList<String> arrayList;
+
+    public BasicRecyclerViewAdapter(ArrayList arrayList) {
+        this.arrayList = arrayList;
+    }
+
+    @Override
+    public BasicViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        // Creamos una vista y le metemos el xml row_basic que es lo que se pasa
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.row_basic,parent,false);
+        return new BasicViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(final BasicViewHolder holder,final int position) {
+        String string = arrayList.get(position);
+        holder.textView.setText(string);
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(v.getContext(),""+holder.getAdapterPosition(),Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return arrayList.size();
+    }
+}
---
+++ CICE/014.6-RecyclerView(InsertRemove)/app/src/main/java/com/example/cice/recyclerview/BasicViewHolder.java
@@ -0,0 +1,27 @@
+package com.example.cice.recyclerview;
+
+import android.content.Intent;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by CICE on 15/12/15.
+ */
+public class BasicViewHolder extends RecyclerView.ViewHolder {
+
+    LinearLayout linearLayout;
+    TextView textView;
+
+    public BasicViewHolder(View row){
+        super(row);
+
+        // Binding con los elementos de la vista.
+        linearLayout = (LinearLayout)row.findViewById(R.id.linear_layout);
+        textView = (TextView)row.findViewById(R.id.text_view);
+
+    }
+}
